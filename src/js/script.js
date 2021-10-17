0ebfc885edf742e286ebec7ce14d841b4cfc23f2
Leerena <leerena7@gmail.com>
2021-10-17 13:05:41 +0300

    added mailer and pageup

--- src/js/script.js
+++ src/js/script.js
@@ -104,4 +104,39 @@ $(document).ready(function(){
 
   $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+    //mailer
+
+    $('form').submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+      }).done(function() {
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+  
+          $('form').trigger('reset');
+      });
+      return false;
+    });
+
+
+    //Smooth Scroll up
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1000) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+
+    $("a[href^='#']").click(function() {
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    })
+
 });

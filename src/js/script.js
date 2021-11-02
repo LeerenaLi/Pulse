f7d208ac9611f737547e2ff99317caacf416bedb
Leerena <leerena7@gmail.com>
2021-11-02 15:58:03 +0300

    added wow.min.js animate.min.css and animation in the section reviews

--- src/js/script.js
+++ src/js/script.js
@@ -137,6 +137,8 @@ $(document).ready(function(){
       const _href = $(this).attr("href");
       $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
       return false;
-    })
+    });
+
+    new WOW().init();
 
 });
